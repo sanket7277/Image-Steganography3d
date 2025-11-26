9464b32cba98a4adbfd09157826e3737ff78ef68
sanket7277 <sanketkolhe5621@gmail.com>
2025-11-27 01:19:42 +0530

    Render DB fix

--- app.py
+++ app.py
@@ -382,7 +382,8 @@ def uploaded_file(filename):
 # ==========================================================
 # RUN APP
 # ==========================================================
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    app.run()
